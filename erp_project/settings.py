2ddfbbc0a1cea33329b9372c82e9d3eb6b7e36ab
unknown <you@example.com>
2025-12-15 20:06:08 -0800

    use secret key from env

--- erp_project/settings.py
+++ erp_project/settings.py
@@ -10,22 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$7@jma=zi*0mh9!ritq3x24wu()jox8&-1egqn&@-iunuhr+98'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
+
 
 
 # Application definition
